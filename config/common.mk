31c2d4f00503b37d5eac09de3b930834f2e0437e
scp1513 <scp1513@gmail.com>
2025-04-04 19:01:24 +0800

    修改默认图标形状为Ios [1/2]
    Change-Id: I0f036856f1b2fa98e6548e41b5d8901d49901149

--- config/common.mk
+++ config/common.mk
@@ -266,6 +266,9 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/crowdin/overlay
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     vendor/lineage/build/target/product/security/lineage
 
+PRODUCT_PACKAGES += \
+    IconShapeRoundOverlay
+
 include vendor/lineage/config/version.mk
 
 -include vendor/lineage-priv/keys/keys.mk
