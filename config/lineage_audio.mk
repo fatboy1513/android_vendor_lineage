06b876307327ffd6e71353eb03920fd1c7e21f97
scp1513 <scp1513@gmail.com>
2025-04-13 22:56:04 +0800

    修改添加铃声
    Change-Id: If1f6edd020361e22039d2a719299b851a9ecff1a

--- config/lineage_audio.mk
+++ config/lineage_audio.mk
@@ -8,26 +8,32 @@ RINGTONE_PATH := vendor/lineage/prebuilt/common/media/audio/ringtones
 
 # Alarms
 PRODUCT_COPY_FILES += \
-    $(ALARM_PATH)/CyanAlarm.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/CyanAlarm.ogg \
-    $(ALARM_PATH)/NuclearLaunch.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/NuclearLaunch.ogg
+    $(ALARM_PATH)/bright_morning.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/bright_morning.ogg \
+    $(ALARM_PATH)/fresh_start.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/fresh_start.ogg \
+    $(ALARM_PATH)/gentle_breeze.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/gentle_breeze.ogg \
+    $(ALARM_PATH)/early_twilight.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/early_twilight.ogg \
+    $(ALARM_PATH)/Dewdrops.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/alarms/Dewdrops.ogg
+
 
 # Notifications
 PRODUCT_COPY_FILES += \
-    $(NOTIFICATION_PATH)/CyanDoink.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/CyanDoink.ogg \
-    $(NOTIFICATION_PATH)/CyanMail.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/CyanMail.ogg \
-    $(NOTIFICATION_PATH)/CyanMessage.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/CyanMessage.ogg \
-    $(NOTIFICATION_PATH)/Laser.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Laser.ogg \
-    $(NOTIFICATION_PATH)/Naughty.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Naughty.ogg \
-    $(NOTIFICATION_PATH)/Pong.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Pong.ogg \
-    $(NOTIFICATION_PATH)/Rang.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Rang.ogg \
-    $(NOTIFICATION_PATH)/Stone.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Stone.ogg
+    $(NOTIFICATION_PATH)/buzz.mp3:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/buzz.mp3 \
+    $(NOTIFICATION_PATH)/Ding.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Ding.ogg \
+    $(NOTIFICATION_PATH)/DingDing.mp3:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/DingDing.mp3 \
+    $(NOTIFICATION_PATH)/droplet.mp3:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/droplet.mp3 \
+    $(NOTIFICATION_PATH)/eureka.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/eureka.ogg \
+    $(NOTIFICATION_PATH)/pulse_enhance.mp3:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/pulse_enhance.mp3 \
+    $(NOTIFICATION_PATH)/pulse.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/pulse.ogg \
+    $(NOTIFICATION_PATH)/Tritone.mp3:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/Tritone.mp3 \
+    $(NOTIFICATION_PATH)/iOS10_Alert1.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/iOS10_Alert1.ogg \
+    $(NOTIFICATION_PATH)/iOS10_Calendar.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/iOS10_Calendar.ogg \
+    $(NOTIFICATION_PATH)/iOS10_Sound10.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/notifications/iOS10_Sound10.ogg
 
 # Ringtones
 PRODUCT_COPY_FILES += \
-    $(RINGTONE_PATH)/Boxbeat.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Boxbeat.ogg \
-    $(RINGTONE_PATH)/CyanTone.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/CyanTone.ogg \
-    $(RINGTONE_PATH)/Highscore.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Highscore.ogg \
-    $(RINGTONE_PATH)/Lyon.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Lyon.ogg \
-    $(RINGTONE_PATH)/Rockin.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Rockin.ogg \
-    $(RINGTONE_PATH)/Sheep.mp3:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Sheep.mp3 \
-    $(RINGTONE_PATH)/Yukaay.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Yukaay.ogg
+    $(RINGTONE_PATH)/the_big_adventure.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/the_big_adventure.ogg \
+    $(RINGTONE_PATH)/your_new_adventure.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/your_new_adventure.ogg \
+    $(RINGTONE_PATH)/marimba_lithe.mp3:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/marimba_lithe.mp3 \
+    $(RINGTONE_PATH)/StartScene.mp3:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/StartScene.mp3 \
+    $(RINGTONE_PATH)/StartScene2.wav:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/StartScene2.wav \
+    $(RINGTONE_PATH)/marimba.m4a:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/marimba.m4a
